de450f84edf41f2b4683b1ac54227ffb90896eb1
jeyasm <142467256+jeyasm@users.noreply.github.com>
2023-08-17 13:27:26 +0800

    Update streamlit_app.py
    update special header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
-streamlit.title('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇'')
+streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Favorites')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
