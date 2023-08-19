177fdb3c526aed04a0137d642b20eb98bcaad45e
jeyasm <142467256+jeyasm@users.noreply.github.com>
2023-08-19 17:28:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,4 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-streamlit.delete("select "jackfruit" from fruit_load_list")
+streamlit.delete(my_cur.jackfruit)
