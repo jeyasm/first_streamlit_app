c136bad26657a318d674ddfaccc3cf70e0e007d5
jeyasm <142467256+jeyasm@users.noreply.github.com>
2023-08-19 18:14:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit + "')")
+
 #New Section to display fruityvice api response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
