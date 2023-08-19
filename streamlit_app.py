af4188f0429e6cc76b5ed2926bc9bc0ed4ad7cc8
jeyasm <142467256+jeyasm@users.noreply.github.com>
2023-08-19 19:37:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,6 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 #allow user to add a fruit
-my_cur.execute("insert into fruit_load_list values ('add_my_fruit = "jackfruit"')")
+my_cur.execute(insert into fruit_load_list values (' + add_my_fruit + ')")
+add_my_fruit = "jackfruit"
 
