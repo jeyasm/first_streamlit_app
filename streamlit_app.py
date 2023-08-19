a2d1621fd9d8fe2495fd4bdc2c9dda54eb4c3afd
jeyasm <142467256+jeyasm@users.noreply.github.com>
2023-08-19 20:26:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,3 +56,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.dataframe(my_data_rows)
 
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
