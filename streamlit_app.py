0f4d7e1d2f77dccf6973b3b3231dd794d643b103
jeyasm <142467256+jeyasm@users.noreply.github.com>
2023-08-19 18:33:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-my_cur.execute("insert into fruit_load_list values('" +add_my_fruit+"')")
+my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit + "')")
 add_my_fruit = "jackfruit"
