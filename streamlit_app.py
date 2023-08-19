dc0b235b58e6cb71ab3960d8c0029efe81bd31f1
jeyasm <142467256+jeyasm@users.noreply.github.com>
2023-08-19 16:15:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,4 +52,4 @@ my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit + "')")
 #allow the end user to add a fruit to the list
 add_my_fruit = "jackfruit"
 
-delete = "jackfruit"
+del "jackfruit"
